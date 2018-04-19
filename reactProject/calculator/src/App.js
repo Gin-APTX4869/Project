af6f5de2b92f9e1915eff51b857b9aec9f5274b4
ko <githubhk@sina.com>
2018-04-19 10:25:02 +0800

    1
    1

--- reactProject/calculator/src/App.js
+++ reactProject/calculator/src/App.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+
+import Panel from 'muicss/lib/react/panel'
+import Calculator from './components/Calculator'
+
+const API_ID = "59598"
+const API_SECRET = "941661ba0a344863850ef3ae3b7d593a"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+      currentRate: '0',
+      count: '0',
+      left: false
+    }
+  }
+
+  componentDidMount = () => {
+    this.getData()
+  }
+
+  getData = () => {
+    fetch(`http://route.showapi.com/105-30?code=&showapi_appid=${API_ID}&showapi_sign=${API_SECRET}`)
+      .then(res => res.json())
+      .then(parsedJSON => parsedJSON.showapi_res_body.list.map(item => ({name: `${item.name}`, zhesuan: `${item.zhesuan}`})))
+      .then(data => this.setState({data}))
+  }
+
+  changeHandler = (e) => {
+    this.setState({currentRate: e.target.value})
+  }
+
+  calculatorHandler = (e) => {
+    e.preventDefault()
+    const price = e.target.elements.price.value
+    const rate = e.target.elements.rate.value
+    const number = e.target.elements.number.value
+    this.setState({
+      count: price * rate / 100 / number
+    })
+  }
+
+  clearHandler = () => {
+    this.setState({count: '0'})
+  }
+
   render() {
+    const {data} = this.state
+    const {currentRate} = this.state
+    const {count} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Panel>
+          当前汇率
+        </Panel>
+        <Panel>
+          <Calculator
+            data={data}
+            currentRate={currentRate}
+            count={count}
+            changeHandler={this.changeHandler}
+            calculatorHandler={this.calculatorHandler}
+            clearHandler={this.clearHandler}/>
+        </Panel>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ reactProject/calculator/src/components/Calculator.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import Option from 'muicss/lib/react/option'
+import Select from 'muicss/lib/react/select'
+import Input from 'muicss/lib/react/input'
+import Button from 'muicss/lib/react/button'
+import Form from 'muicss/lib/react/form'
+
+const Calculator = (props) => {
+  return (
+    <div>
+      <Form onSubmit={props.calculatorHandler}>
+        <Input label="买入价" name="price" floatingLabel={true}/>
+        <Select name="input" label="选择币种" onChange={props.changeHandler}>
+          {props
+            .data
+            .map((item, index) => {
+              const {zhesuan, name} = item
+              return (<Option key={index} value={zhesuan} label={name}/>)
+            })
+          }
+        </Select>
+        <Input
+          label="当前汇率"
+          name="rate"
+          floatingLabel={true}
+          value={props.currentRate}
+          disabled/>
+        <Input label="个数" name="number" floatingLabel={true}/>
+        <Button variant="raised" color="primary">计算</Button>
+      </Form>
+      <Button variant="raised" onClick={props.clearHandler}>清零</Button>
+      <div className="mui--text-caption">折合人民币：{props.count}</div>
+    </div>
+  )
+}
+
+export default Calculator
